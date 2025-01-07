530cc3e3d166f6373fe456994eabbcbcd5fbf2fe
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-01-07 14:32:38 -0300

    :sparkles: feat: Creating first view function returning a JSON

--- apps/school/views.py
+++ apps/school/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 # Create your views here.
+
+def estudantes(request):
+    estudantes={
+        "id":1,
+        "nome":"Lucas Marciano"
+    }
+    return JsonResponse(estudantes)
