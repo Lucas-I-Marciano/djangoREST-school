b10a1b497bca1fc632febd2c45b9b196b7932f06
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-01-08 19:49:44 -0300

    :sparkles: feat: Creating course router

--- apps/school/urls.py
+++ apps/school/urls.py
@@ -3,9 +3,10 @@ from apps.school.views import estudantes
 
 from rest_framework import routers
 
-from apps.school.views import EstudanteViewSet
+from apps.school.views import EstudanteViewSet, CursoViewSet
 
 router = routers.DefaultRouter()
 router.register('estudantes', EstudanteViewSet)
+router.register('cursos', CursoViewSet, basename="Cursos")
 
 urlpatterns = router.urls
