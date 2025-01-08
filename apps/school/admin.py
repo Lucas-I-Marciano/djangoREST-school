0e654413ef55b82777b8a1a8aa4f70d8ef3bd23d
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-01-08 12:31:39 -0300

    :sparkles: feat: Registering student model on admin site

--- apps/school/admin.py
+++ apps/school/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from apps.school.models import Estudante
+
 # Register your models here.
+
+class EstudanteAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Estudante, EstudanteAdmin)
