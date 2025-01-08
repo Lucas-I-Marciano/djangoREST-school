a0d729bbdcd5640d49843f34cb770327a2af70b7
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-01-08 12:47:53 -0300

    :sparkles: feat: Adjusting how admin page will be showed for studant model

--- apps/school/admin.py
+++ apps/school/admin.py
@@ -5,7 +5,12 @@ from apps.school.models import Estudante, Curso
 # Register your models here.
 
 class EstudanteAdmin(admin.ModelAdmin):
-    pass
+    empty_value_display = "-?-"
+    list_display  = ['id', 'nome', 'email', 'cpf', 'data_nascimento', 'celular']
+    list_display_links = ['id', 'nome']
+    list_editable = ['celular']
+    list_per_page = 20
+    search_fields = ['nome']
 
 admin.site.register(Estudante, EstudanteAdmin)
 
