e6ee89f3d06e2e03de90ccb0333c6e1ad5c11c01
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-01-09 13:18:33 -0300

    :sparkles: feat: Adding tutorial app on project

--- setup/settings.py
+++ setup/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'apps.school.apps.SchoolConfig',
-    'rest_framework'
+    'rest_framework',
+    'snippets'
 ]
 
 MIDDLEWARE = [
