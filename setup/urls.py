0229f6cc9b2c4f933499c09450cb16cf783e5cda
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-01-09 18:09:58 -0300

    :sparkles: feat: Adding urls from my snippet app into project

--- setup/urls.py
+++ setup/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('apps.school.urls')),
     path('api-auth/', include('rest_framework.urls')),
+    path('', include('apps.snippets.urls'))
 ]
