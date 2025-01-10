04bc7d27beec2067e57f4bd6ca473fc561309b79
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-01-10 13:11:25 -0300

    :sparkles: feat: Adding mixin classes to get use of reusable bits of behavior of operations used before
    Attributes from GenericAPIView: queryset - The queryset that should be used for returning objects from this view; serializer_class - The serializer class that should be used for validating and deserializing input

--- apps/snippets/views.py
+++ apps/snippets/views.py
@@ -3,29 +3,31 @@ from django.http import Http404
 from apps.snippets.models import Snippet
 from apps.snippets.serializers import SnippetSerializer
 
-from rest_framework import status, permissions
+from rest_framework import status, permissions, mixins, generics
 from rest_framework.decorators import permission_classes
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 
+
+
 # Create your views here.
 @permission_classes((permissions.AllowAny,))
-class SnippetList(APIView):
+class SnippetList(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    generics.GenericAPIView
+    ):
     """
     List all snippets, or create a new snippet.
     """
-    def get(self, request):
-        snippets = Snippet.objects.all()
-        serializer = SnippetSerializer(snippets, many=True)
-        return Response(serializer.data)
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
 
-    def post(self, request):
-        serializer = SnippetSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
 
 
 @permission_classes((permissions.AllowAny,))
