3073ace50c75c9a272d133b06dd0c028cfd434ac
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-01-11 14:36:40 -0300

    :sparkles: feat: Adding which field will be displayed by owner and a custom field from user model

--- apps/snippets/serializers.py
+++ apps/snippets/serializers.py
@@ -3,6 +3,8 @@ from apps.snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 from django.contrib.auth import models
 
 class SnippetSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    owner_last_login = serializers.ReadOnlyField(source='owner.last_login')
     class Meta:
         model = Snippet
         exclude = ['highlighted']
