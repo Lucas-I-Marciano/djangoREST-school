183481b3f25c2d26564cbf2468ac302cfd8bc7be
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-01-12 20:53:02 -0300

    :sparkles: feat: Changing list of user from normal view to ViewSet approach

--- apps/snippets/urls.py
+++ apps/snippets/urls.py
@@ -2,10 +2,14 @@ from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 from apps.snippets import views
 
+user_list = views.UserViewSet.as_view({
+    'get':'list'
+})
+
 urlpatterns = [
     path('snippets/', views.SnippetList.as_view(), name='snippet-list'),
     path('snippets/<int:pk>/', views.SnippetDetail.as_view(), name='snippet-detail'),
-    path('users/', views.UserList.as_view(), name='user-list'),
+    path('users/', user_list, name='user-list'),
     path('users/<int:pk>/', views.UserDetail.as_view(), name='user-detail'),
     path('', views.api_root),
     path('snippets/<int:pk>/highlight/', views.SnippetHighlight.as_view(), name='snippet-highlight')
