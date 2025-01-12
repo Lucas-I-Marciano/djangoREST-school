a85687d495b976733ec594f502a969651503adba
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-01-12 20:55:08 -0300

    :sparkles: feat: Changing retrieving of user from normal view to ViewSet approach

--- apps/snippets/urls.py
+++ apps/snippets/urls.py
@@ -6,11 +6,15 @@ user_list = views.UserViewSet.as_view({
     'get':'list'
 })
 
+user_detail = views.UserViewSet.as_view({
+    'get' : 'retrieve',
+})
+
 urlpatterns = [
     path('snippets/', views.SnippetList.as_view(), name='snippet-list'),
     path('snippets/<int:pk>/', views.SnippetDetail.as_view(), name='snippet-detail'),
     path('users/', user_list, name='user-list'),
-    path('users/<int:pk>/', views.UserDetail.as_view(), name='user-detail'),
+    path('users/<int:pk>/', user_detail, name='user-detail'),
     path('', views.api_root),
     path('snippets/<int:pk>/highlight/', views.SnippetHighlight.as_view(), name='snippet-highlight')
 ]
