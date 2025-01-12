620fe2aa46edc0fd19cfd558ea027c546cbaeef5
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-01-12 20:41:42 -0300

    :sparkles: feat: Changing how permission is implemented

--- apps/snippets/views.py
+++ apps/snippets/views.py
@@ -50,17 +50,18 @@ class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     def delete(self, request, *args, **kwargs):
         return self.destroy(request, *args, **kwargs)
 
-@permission_classes([permissions.IsAuthenticatedOrReadOnly])
+
 class UserList(generics.ListAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     
 
 
-@permission_classes((permissions.AllowAny,))
 class UserDetail(generics.RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
 
 @api_view(['GET'])
