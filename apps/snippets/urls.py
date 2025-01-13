c0939617d4aa5c607cc1c4d575af81acdeacad84
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-01-13 17:54:04 -0300

    :sparkles: feat: Adding view for highlighted code
    Note that on 'GET' HTTP verb, it will perform a action that is not the standard ones - list/create/retrieve/update/partial_update/destroy

--- apps/snippets/urls.py
+++ apps/snippets/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 from apps.snippets import views
 
+from rest_framework import renderers
+
 user_list = views.UserViewSet.as_view({
     'get':'list'
 })
@@ -22,13 +24,17 @@ snippet_detail = views.SnippetViewSet.as_view({
     'delete' : 'destroy'
 })
 
+snippet_highlight = views.SnippetViewSet.as_view({
+    'get': 'highlight'
+}, renderer_classes=[renderers.StaticHTMLRenderer])
+
 urlpatterns = [
     path('snippets/', snippet_list, name='snippet-list'),
     path('snippets/<int:pk>/', snippet_detail, name='snippet-detail'),
     path('users/', user_list, name='user-list'),
     path('users/<int:pk>/', user_detail, name='user-detail'),
     path('', views.api_root),
-    path('snippets/<int:pk>/highlight/', views.SnippetHighlight.as_view(), name='snippet-highlight')
+    path('snippets/<int:pk>/highlight/', snippet_highlight, name='snippet-highlight')
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
