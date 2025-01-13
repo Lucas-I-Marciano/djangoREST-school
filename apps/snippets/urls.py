4d4af0cf5ecb26b9e286927f6f550fc761a62851
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-01-13 19:56:43 -0300

    :sparkles: feat: Adding snippets router to app urls

--- apps/snippets/urls.py
+++ apps/snippets/urls.py
@@ -7,28 +7,10 @@ from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register(r'users', views.UserViewSet, basename='user')
-
-snippet_list = views.SnippetViewSet.as_view({
-    'get' : 'list',
-    'post' : 'create'
-})
-
-snippet_detail = views.SnippetViewSet.as_view({
-    'get' : 'retrieve',
-    'put' : 'update',
-    'patch' : 'partial_update',
-    'delete' : 'destroy'
-})
-
-snippet_highlight = views.SnippetViewSet.as_view({
-    'get': 'highlight'
-}, renderer_classes=[renderers.StaticHTMLRenderer])
+router.register(r'snippets', views.SnippetViewSet, basename='snippetsss')
 
 urlpatterns = [
-    path('snippets/', snippet_list, name='snippet-list'),
-    path('snippets/<int:pk>/', snippet_detail, name='snippet-detail'),
     path('', include(router.urls)),
-    path('snippets/<int:pk>/highlight/', snippet_highlight, name='snippet-highlight')
 ]
 
 # urlpatterns = format_suffix_patterns(urlpatterns)
