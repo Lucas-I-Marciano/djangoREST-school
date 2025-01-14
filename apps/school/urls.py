b1b61df7f77bb2c8052eb491b3bd8ae592a7c809
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-01-14 12:59:49 -0300

    :sparkles: feat: Including URL with 'pk' for custom View

--- apps/school/urls.py
+++ apps/school/urls.py
@@ -3,7 +3,7 @@ from apps.school.views import estudantes
 
 from rest_framework import routers
 
-from apps.school.views import EstudanteViewSet, CursoViewSet, MatriculaViewSet
+from apps.school.views import EstudanteViewSet, CursoViewSet, MatriculaViewSet, MatriculasEstudantesView
 
 router = routers.DefaultRouter()
 router.register('estudantes', EstudanteViewSet)
@@ -11,5 +11,6 @@ router.register('cursos', CursoViewSet, basename="Cursos")
 router.register('matriculas', MatriculaViewSet, basename="Matriculas")
 
 urlpatterns = [
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    path('estudantes/<int:pk>/matriculas/', MatriculasEstudantesView.as_view())
 ]
