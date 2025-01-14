f83960058738816241e2f5e07583628aeee5216b
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-01-14 20:12:28 -0300

    :sparkles: feat: Creating validation on 'cpf' field
    Unique field

--- apps/school/models.py
+++ apps/school/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Estudante(models.Model):
     nome = models.CharField(max_length=100)
     email = models.EmailField(max_length=30, blank=False)
-    cpf = models.CharField(max_length=11)
+    cpf = models.CharField(max_length=11, unique=True)
     data_nascimento = models.DateField()
     celular = models.CharField(max_length=14)
 
