bd92854f64782598f7dbdd87925e9a664ef50a8a
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-01-14 13:09:39 -0300

    :sparkles: feat: Including URL with 'pk' to show enrollments by course

--- apps/school/urls.py
+++ apps/school/urls.py
@@ -3,7 +3,7 @@ from apps.school.views import estudantes
 
 from rest_framework import routers
 
-from apps.school.views import EstudanteViewSet, CursoViewSet, MatriculaViewSet, MatriculasEstudantesView
+from apps.school.views import EstudanteViewSet, CursoViewSet, MatriculaViewSet, MatriculasEstudantesView, MatriculasCursosView
 
 router = routers.DefaultRouter()
 router.register('estudantes', EstudanteViewSet)
@@ -12,5 +12,6 @@ router.register('matriculas', MatriculaViewSet, basename="Matriculas")
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('estudantes/<int:pk>/matriculas/', MatriculasEstudantesView.as_view())
+    path('estudantes/<int:pk>/matriculas/', MatriculasEstudantesView.as_view()),
+    path('cursos/<int:pk>/matriculas/', MatriculasCursosView.as_view()),
 ]
