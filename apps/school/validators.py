801d752a6b1d394250df1b2d12980fa7d8854f9d
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-01-15 19:47:30 -0300

    :recycle: refactor: Validanting tax id from 3rd party library

--- apps/school/validators.py
+++ apps/school/validators.py
@@ -1,8 +1,11 @@
+from validate_docbr import CPF
+
 def nome_invalido(value):
     return not value.replace(' ', '').isalpha()
 
 def cpf_invalido(value):
-    return len(value) != 11
+    cpf = CPF()
+    return not cpf.validate(str(value))
 
 def celular_invalido(value):
     return len(value) != 13
