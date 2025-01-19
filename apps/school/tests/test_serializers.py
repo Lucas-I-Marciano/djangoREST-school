436639ed6d77259b13cf1ab2e61759988aa7669b
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-01-19 07:41:57 -0300

    :test_tube: test: Testing student field values from serializer

--- apps/school/tests/test_serializers.py
+++ apps/school/tests/test_serializers.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from apps.school.serializers import EstudanteSerializer
 from apps.school.models import Estudante
+from datetime import date
 
 
 class SerializerEstudanteTestCase(TestCase):
@@ -16,3 +17,10 @@ class SerializerEstudanteTestCase(TestCase):
 
     def test_fields_student_serializer(self):
         self.assertEqual(set(self.serializer_estudante.data.keys()), set(['id', 'nome', 'email', 'cpf', 'data_nascimento', 'celular']))
+
+    def test_field_value_student_serializer(self):
+        self.assertEqual(self.serializer_estudante.data['nome'], "Lucas Ioran Marciano")
+        self.assertEqual(self.serializer_estudante.data['email'], "l@l.com")
+        self.assertEqual(self.serializer_estudante.data['cpf'], "80597057095")
+        self.assertEqual(self.serializer_estudante.data['data_nascimento'], "1999-01-07")
+        self.assertEqual(self.serializer_estudante.data['celular'], "89 99999 9999")
