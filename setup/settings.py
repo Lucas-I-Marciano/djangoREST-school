aef423504cd35381ace7d38dd9c1818533541ff4
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-01-20 17:12:16 -0300

    :sparkles: feat: Adding settings from CORS headers to frontend get data from server

--- setup/settings.py
+++ setup/settings.py
@@ -42,9 +42,11 @@ INSTALLED_APPS = [
     'apps.snippets.apps.SnippetsConfig',
     'django_filters',
     'drf_yasg',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -146,3 +148,8 @@ REST_FRAMEWORK = {
         'user': '12/minute'
     }
 }
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8042",
+    "http://127.0.0.1:8042",
+]
