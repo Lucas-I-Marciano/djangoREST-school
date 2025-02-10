b7adfc4dbe269f97abc01f66c742a136d88086d2
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-02-10 19:39:14 -0300

    :test_tube: test: Testing creation and its data

--- apps/school/tests/routes/test_cursos.py
+++ apps/school/tests/routes/test_cursos.py
@@ -4,6 +4,8 @@ from django.urls import reverse
 from rest_framework.test import APITestCase
 from rest_framework import status
 
+from apps.school.models import Curso
+
 class test_CursosTestCase(APITestCase):
     def setUp(self):
         self.url_list_course = reverse('Cursos-list')
@@ -32,3 +34,4 @@ class test_CursosTestCase(APITestCase):
         self.client.force_authenticate(self.superuser)
         response = self.client.post(self.url_list_course, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Curso.objects.get().codigo, data["codigo"])
