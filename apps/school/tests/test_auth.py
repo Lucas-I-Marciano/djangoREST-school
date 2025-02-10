1217ebf744990a36bb8c571520eba9f0f037f0db
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-02-10 17:15:12 -0300

    :test_tube: test: Testing correct authentication

---
+++ apps/school/tests/test_auth.py
@@ -0,0 +1,11 @@
+from django.test import TestCase
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+
+class test_AutenticacaoUsuario(TestCase):
+    def setUp(self):
+        self.superuser = User.objects.create_superuser("admin", "l@l.com", "test123")
+
+    def test_correct_authentication(self):
+        user = authenticate(username="admin", password="test123")
+        self.assertTrue(user)
