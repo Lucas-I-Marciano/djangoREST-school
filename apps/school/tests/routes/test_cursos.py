e4c05e946dd443b1faa5d4a899909b500822f280
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-02-10 20:10:39 -0300

    :test_tube: test: Testing couse updating

--- apps/school/tests/routes/test_cursos.py
+++ apps/school/tests/routes/test_cursos.py
@@ -47,3 +47,17 @@ class test_CursosTestCase(APITestCase):
         url_delete = f'{self.url_list_course}{response.data['id']}/'
         response = self.client.delete(url_delete)
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_update_course(self):
+        data = {
+            "codigo": "JS2",
+            "descricao": "JavaScript2",
+            "nivel": "B"
+        }
+        self.client.force_authenticate(self.superuser)
+        response = self.client.post(self.url_list_course, data, format='json')
+        url_update = f'{self.url_list_course}{response.data['id']}/' #/estudantes/1
+        new_data = {**data}
+        new_data['codigo'] = "JavaS2"
+        response = self.client.put(url_update, new_data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
